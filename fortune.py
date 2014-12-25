6df4494b8005b313422b6d72f646ecd2495f3f0f
Roger Pate <roger@qxxy.com>
2014-12-25 09:51:20 -0500

    clean up options

--- fortune.py
+++ fortune.py
@@ -13,72 +13,46 @@ from pprint import pprint
 import yaml
 
 
-if sys.stdout.encoding is None:
-    sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout, errors="replace")
-
-
 Quote = namedtuple("Quote", ["text", "attribution", "ref", "tags", "notes"])
 
-
 def _main(args):
-    optparser = optparse.OptionParser(usage="%prog [OPTION..] [QUOTE FILE..]")
-    optparser.add_option("-w", "--width", type="int", default=79)
-    optparser.add_option("--max-width", type="int", default=None, help="width = min(width, max_width)")
-    optparser.add_option("-v", "--verbose", action="store_true", default=False)
-    optparser.add_option("--qa-only", dest="qa_only", action="store_true", default=False,
-        help="show quote and author/byline only")
-    optparser.add_option("--notes", dest="show_notes", action="store_true", default=False)
-    optparser.add_option("--debug", action="store_true", default=False)
-    optparser.add_option("--grep", nargs=1, default=None)
-    optparser.add_option("--no-wrap", dest="wrap", action="store_false", default=True)
-    optparser.add_option("--blank-line", action="store_true", default=False,
-        help="output a blank line between each quote")
-    optparser.add_option("-1", "--oneline", action="store_true", default=False,
-        help="(implies no-wrap)")
-    optparser.add_option("--all", action="store_true", default=False,
-        help="show all")
+    if sys.stdout.encoding is None:
+        sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout, errors="replace")
+
+    optparser = optparse.OptionParser(usage="%prog [OPTION..] [QUOTE_FILE..]", add_help_option=False)
     optparser.add_option("-n", type="int", default=None,
-        help="run N times")
+        help="show N quotes")
+    optparser.add_option("--grep", nargs=1, default=None,
+        help="show quotes matching pattern")
+    optparser.add_option("--notes", dest="show_notes", action="store_true", default=False,
+        help="include notes")
+    optparser.add_option("--width", type="int", default=79,
+        help="wrap output lines, or 0 to disable [default %default]")
+    optparser.add_option("--no-blank-line", dest="blank_line", action="store_false", default=True,
+        help="don't output a blank line between each quote")
+    optparser.add_option("--help", action="help",
+        help="show this help and exit")
+    optparser.add_option("--debug", action="store_true", default=False)
     opts, args = optparser.parse_args(args)
 
-    if opts.qa_only and opts.show_notes:
-        optparser.error("cannot combine --qa-only with --notes")
-    if opts.all:
-        if opts.n is not None:
-            optparser.error("cannot combine --all with -n")
-        if opts.grep is not None:
-            optparser.error("cannot combine --all with --grep")
     if opts.n is None and opts.grep is None:
         opts.n = 1
     if opts.n is not None and opts.n < 1:
         optparser.error("invalid -n value")
 
-    if opts.max_width:
-        opts.width = min(opts.width, opts.max_width)
+    if not args:
+        args = glob.glob(path.join(path.dirname(__file__), "quotes*.yaml"))
 
-    if opts.oneline:
-        opts.wrap = False
-
-    if opts.verbose:
-        opts.show_notes = True
-
-    if args:
-        filenames = args
-    else:
-        filenames = glob.glob(path.join(path.dirname(__file__), "quotes*.yaml"))
-
-    if opts.debug or opts.verbose:
+    if opts.debug:
         print "opts:", repr(opts)
         print "args:", repr(args)
-        print "filenames:", repr(filenames)
-        if opts.debug:
-            return
+        print "filenames:", repr(args)
 
     quotes = []
-    for filename in filenames:
+    for filename in args:
         if filename.startswith("-"):
             return "unexpected option"
-        collection = path.splitext(path.basename(filename))[0]
+        collection = path.basename(filename)
         for doc in yaml.load_all(codecs.open(filename, encoding="utf-8", errors="replace")):
             q = doc.get("q")
             if "title" in doc:
@@ -86,7 +60,7 @@ def _main(args):
             elif "by" in doc:
                 a = doc["by"]
                 source = doc.get("source")
-                if source and not opts.qa_only:
+                if source:
                     a += ", " + source
             elif "source" in doc:
                 a = doc["source"]
@@ -114,39 +88,35 @@ def _main(args):
             q = Quote(q, a, ref, tags, notes)
             quotes.append((collection, q))
 
-    if opts.verbose:
-        print "[loaded %d quotes from %d files]" % (len(quotes), len(filenames))
+    if opts.debug:
+        print "[loaded {} quotes from {} files]".format(len(quotes), len(filenames))
 
-    _show_quote_first = [True]
-    def show_quote(q, collection):
-        if _show_quote_first[0]:
-            _show_quote_first[0] = False
-        elif opts.blank_line:
+    def show_quote(q, collection, is_first):
+        if not is_first and opts.blank_line:
             print
 
-        if opts.verbose:
+        if opts.debug:
             print "quote =", q
         text = q.text.strip()
-        if opts.wrap:
+        if opts.width:
             text = textwrap.fill(text, width=opts.width)
+        print text
 
         attr = q.attribution or "(unknown)"
-        if q.ref and not opts.qa_only:
+        if q.ref:
             attr += "; " + q.ref
 
-        if opts.qa_only:
-            end_text = "-- " + attr
-        else:
-            end_text = u"-- %s [%s]" % (attr, collection)
+        end_text = u"  -- {}".format(attr)
+        if opts.show_notes:
+            end_text += u" [{}]".format(collection)
+        print end_text
 
-        sep = " " if opts.oneline else "\n  "
-        print u"%s%s%s" % (text, sep, end_text)
         if opts.show_notes:
             for note in q.notes:
                 print "#", note
 
     if opts.grep:
-        if re.escape(opts.grep) == opts.grep:  # not a regex, could be improved
+        if re.escape(opts.grep) == opts.grep:  # test if a fixed-string match is possible, could be improved
             opts.grep = opts.grep.lower()
             def grep(s):
                 if not isinstance(s, basestring):
@@ -160,21 +130,17 @@ def _main(args):
         count = 0
         for collection, q in quotes:
             if any(grep(t) for t in q if t):
-                show_quote(q, collection)
+                show_quote(q, collection, count == 0)
                 count += 1
                 if opts.n is not None and count == opts.n:
                     break
     else:
-        if opts.all:
-            for collection, q in quotes:
-                show_quote(q, collection)
-        else:
-            for _ in xrange(opts.n):
-                collection, q = random.choice(quotes)
-                show_quote(q, collection)
+        for count in xrange(opts.n):
+            collection, q = random.choice(quotes)
+            show_quote(q, collection, count == 0)
 
 def main(args):
     try:
         return _main(args)
     except KeyboardInterrupt:
-        pass
+        print
